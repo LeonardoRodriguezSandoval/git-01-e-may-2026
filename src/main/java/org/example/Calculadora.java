9918f030fecadba283741550514cf1e929b8c2b7
Leonardo Rodriguez Sandoval <20253DS137@utez.edu.mx>
2026-05-19 13:28:20 -0600

    [NEW] creacion de clase calculadora y metodo sumar

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculadora {
+
+    private double numero1;
+    private double numero2;
+
+    public Calculadora(){
+
+
+    }
+
+    public Calculadora (double numero1, double numero2){
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public double sumar(){
+        return numero1 + numero2;
+    }
+}
